f14aeec79f058e2b9c866c36d4d24167e34861ba
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-03-26 22:13:28 -0300

    adiciona proximo

--- proximo.test.js
+++ proximo.test.js
@@ -1,7 +1,7 @@
 import { test, expect} from 'vitest'
-
+import { proximo } from './proximo'
 test('proximo(0) retorna 1', () => {
-    expect(proximo(0)).toBe(3)
+    expect(proximo(0)).toBe(1)
 })
 test('proximo(1) retorna 3', () => {
     expect(proximo(1)).toBe(3)
